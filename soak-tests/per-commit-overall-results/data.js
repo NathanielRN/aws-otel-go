380ca119b7563596dbca40cc821d96258fbfd881
github-action-benchmark <github@users.noreply.github.com>
2021-09-22 20:13:29 +0000

    add Soak Test Results - sample-app-golang_alpine-manual (custombenchmark) benchmark result for 9689cfc20b3d28ee7911cf5196e34f45ecf1d053

---
+++ soak-tests/per-commit-overall-results/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1632341609739,
+  "repoUrl": "https://github.com/NathanielRN/aws-otel-go",
+  "entries": {
+    "Soak Test Results - sample-app-golang_alpine-manual": [
+      {
+        "commit": {
+          "author": {
+            "name": "(Eliseo) Nathaniel Ruiz Nowell",
+            "username": "NathanielRN",
+            "email": "nathanielruiz98@gmail.com"
+          },
+          "committer": {
+            "name": "(Eliseo) Nathaniel Ruiz Nowell",
+            "username": "NathanielRN",
+            "email": "nathanielruiz98@gmail.com"
+          },
+          "id": "9689cfc20b3d28ee7911cf5196e34f45ecf1d053",
+          "message": "Modify the Soak Tests for ADOT Go Downstream",
+          "timestamp": "2021-09-20T22:57:30Z",
+          "url": "https://github.com/NathanielRN/aws-otel-go/commit/9689cfc20b3d28ee7911cf5196e34f45ecf1d053"
+        },
+        "date": 1632341609330,
+        "tool": "custombenchmark",
+        "benches": [
+          {
+            "name": "Soak Test Average CPU Load",
+            "value": 30.763541666666665,
+            "unit": "Percent"
+          },
+          {
+            "name": "Soak Test Average Virtual Memory Used",
+            "value": 700.484375,
+            "unit": "Megabytes"
+          }
+        ]
+      }
+    ]
+  }
+}
