87e27553ab4e177bbb98c64777f1eaed026d632d
github-action-benchmark <github@users.noreply.github.com>
2021-09-23 20:13:10 +0000

    add Soak Test Results - sample-app-golang_alpine-manual (custombenchmark) benchmark result for e74971a15c3fe7ea4dd20c866041df87e36db6da

--- soak-tests/per-commit-overall-results/data.js
+++ soak-tests/per-commit-overall-results/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632341609739,
+  "lastUpdate": 1632427990742,
   "repoUrl": "https://github.com/NathanielRN/aws-otel-go",
   "entries": {
     "Soak Test Results - sample-app-golang_alpine-manual": [
@@ -26,6 +26,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "Soak Test Average CPU Load",
             "value": 30.763541666666665,
+            "unit": "Percent"
+          },
+          {
+            "name": "Soak Test Average Virtual Memory Used",
+            "value": 700.484375,
+            "unit": "Megabytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "(Eliseo) Nathaniel Ruiz Nowell",
+            "username": "NathanielRN",
+            "email": "nathanielruiz98@gmail.com"
+          },
+          "committer": {
+            "name": "(Eliseo) Nathaniel Ruiz Nowell",
+            "username": "NathanielRN",
+            "email": "nathanielruiz98@gmail.com"
+          },
+          "id": "e74971a15c3fe7ea4dd20c866041df87e36db6da",
+          "message": "Add DEBUG Log Level to integration workflow",
+          "timestamp": "2021-09-23T17:21:16Z",
+          "url": "https://github.com/NathanielRN/aws-otel-go/commit/e74971a15c3fe7ea4dd20c866041df87e36db6da"
+        },
+        "date": 1632427990172,
+        "tool": "custombenchmark",
+        "benches": [
+          {
+            "name": "Soak Test Average CPU Load",
+            "value": 31.667557471264367,
             "unit": "Percent"
           },
           {
